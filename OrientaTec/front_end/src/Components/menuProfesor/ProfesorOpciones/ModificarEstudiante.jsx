343cb724a0f766ab76fa318ab6fe669acb08a0b2
Alonso Garita <45644676+alonso-gg@users.noreply.github.com>
2023-05-01 21:48:26 -0600

    No me acuerdo de qué cambié

--- OrientaTec/front_end/src/Components/menuProfesor/ProfesorOpciones/ModificarEstudiante.jsx
+++ OrientaTec/front_end/src/Components/menuProfesor/ProfesorOpciones/ModificarEstudiante.jsx
@@ -3,7 +3,7 @@ import { Navbar } from '../../navegacion/Navbar'
 import { BarraLateral } from '../../navegacion/BarraLateral'
 import { Icon } from '@iconify/react';
 import { useNavigate } from "react-router-dom";
-export  function ModificarEstudiante() {
+export function ModificarEstudiante() {
     let navigate = useNavigate();
 
     const gotoMenuProfesor = () => { navigate("/menuProfesor", {}); };
@@ -16,9 +16,9 @@ export  function ModificarEstudiante() {
     const [sede, setSede] = useState('');
 
     const handleSubmit = (event) => {
-        event.preventDefault();  
+        event.preventDefault();
         // Aquí puedes enviar los datos a tu backend o hacer lo que necesites con ellos
-      }
+    }
     const handleSearch = () => {
         // Aquí podrías agregar la lógica para buscar la información
         // y asignarla 
@@ -33,30 +33,30 @@ export  function ModificarEstudiante() {
 
     const handleNameChange = (event) => {
         setName(event.target.value);
-        }; 
-        
-    const handleApellido1Change = (event) => { 
+    };
+
+    const handleApellido1Change = (event) => {
         setApellido1(event.target.value);
-        }; 
+    };
     const handleApellido2Change = (event) => {
         setApellido2(event.target.value);
-        };  
+    };
     const handleCelularChange = (event) => {
         setNumeroTelefono(event.target.value);
-    }; 
+    };
     const handleCorreoChange = (event) => {
         setCorreo(event.target.value);
-    }; 
-    
+    };
+
     const handleSedeChange = (event) => {
         setSede(event.target.value);
     };
     const handleEstadoChange = (event) => {
         setEstado(event.target.value);
     };
-  return (
-    <Fragment>
-        <div className="container">
+    return (
+        <Fragment>
+            <div className="container">
                 <Navbar />
                 <div class="row">
                     <div class="col-sm-3">
@@ -64,7 +64,7 @@ export  function ModificarEstudiante() {
                     </div>
                     <div class="col-lg m-3 p-3 bg-light">
                         <h4>Modificar información de un estudiante</h4>
-                         {/*La seguiente seccion mostrara un input group donde se introducira el codigo
+                        {/*La seguiente seccion mostrara un input group donde se introducira el codigo
                          y porsterior se tocara el boton */}
                         <div className="input-group w-50 my-3">
                             <span className="input-group-text" >Carnet</span>
@@ -74,84 +74,70 @@ export  function ModificarEstudiante() {
                         {/*La seguiente seccion tendra los formGroupInput, al tocar el boton de buscar 
                         se rellera con la informacion del estudiante que buscar */}
                         <form onSubmit={handleSubmit}>
-                        <div>
-                            <div class="mb-3">
+                            <div>
                                 <div class="mb-3">
-                                    <label htmlFor="formGroupInputName" class="form-label">Nombre</label>
-                                    <input type="text" className="form-control" id="nameInput"
+                                    <div class="mb-3">
+                                        <label htmlFor="formGroupInputName" class="form-label">Nombre</label>
+                                        <input type="text" className="form-control" id="nameInput"
                                             placeholder="Ingrese su nombre" value={name} onChange={handleNameChange}
-                                            />
-                                </div>
-                                <div class="mb-3">
-                                    <label htmlFor="formGroupInputApellido1" class="form-label">Primer Apellido</label>
-                                    <input type="text" class="form-control" id="formGroupInputApellido1" placeholder="Ingrese su primer apellido"
-                                    value={apellido1} onChange={handleApellido1Change}/>
-                                </div>
-                                <div class="mb-3">
-                                    <label htmlFor="formGroupInputApellido2" class="form-label">Segundo Apellido</label>
-                                    <input type="text" className="form-control" id="nameInput"
+                                        />
+                                    </div>
+                                    <div class="mb-3">
+                                        <label htmlFor="formGroupInputApellido1" class="form-label">Primer Apellido</label>
+                                        <input type="text" class="form-control" id="formGroupInputApellido1" placeholder="Ingrese su primer apellido"
+                                            value={apellido1} onChange={handleApellido1Change} />
+                                    </div>
+                                    <div class="mb-3">
+                                        <label htmlFor="formGroupInputApellido2" class="form-label">Segundo Apellido</label>
+                                        <input type="text" className="form-control" id="nameInput"
                                             placeholder="Ingrese su segundo apellido" value={apellido2} onChange={handleApellido2Change}
-                                            />
-                                </div>
-                                
-                                <div class="mb-3">
-                                    <label htmlFor="formGroupInputNumeroCelular" class="form-label">Número de celular</label>
-                                    <input type="text" className="form-control" id="nameInput"
-                                            placeholder="Ingrese su número de telefono" value={numeroTelefono} onChange={handleCelularChange}
-                                            />
-                                </div>
-                                
-                                <div class="mb-3">
-                                    <label htmlFor="formGroupInputCorreo" class="form-label">Correo</label>
-                                    <input type="text" className="form-control" id="nameInput"
-                                            placeholder="Ingrese su correo" value={correo} onChange={handleCorreoChange}
-                                            />
-                                </div>
-                                <div class="mb-3">
-                                    <label htmlFor="autoSizingSelect" class="form-label">Sede: </label>
-                                    
-                                </div>
-                                <div class="mb-3">
-                                    <select id="mySelect" value={sede} onChange={handleSedeChange}>
-                                    <option value="">Seleccionar</option>
-                                    <option value="1">Cartago</option>
-                                    <option value="2">Alajuela</option>
-                                    <option value="3">Limon</option>
-                                    </select>
-                                </div>
-                                <div class="mb-3">
-                                    <label htmlFor="autoSizingEstado" class="form-label">Estado: </label>
-                                    
-                                </div>
-                                <div class="mb-3">
-                                    <select id="mySelect2" value={estado} onChange={handleEstadoChange}>
-                                    <option value="">Seleccionar</option>
-                                    <option value="1">Activo</option>
-                                    <option value="2">Inactivo</option>
-                                    </select>
-                                </div>
-                                <div class="row m-5">
-                                    <div class="col">
-                                    <button type="submit" class="btn btn-primary">Modificar información</button>
-                                    
+                                        />
                                     </div>
-                                    <div class="col">
-                                        <button type="button" class="btn btn-primary" onClick={gotoMenuProfesor}>
-                                            Atrás
-                                        </button>
-                                    </div>
-                                </div>
 
+                                    <div class="mb-3">
+                                        <label htmlFor="formGroupInputNumeroCelular" class="form-label">Número de celular</label>
+                                        <input type="text" className="form-control" id="nameInput"
+                                            placeholder="Ingrese su número de telefono" value={numeroTelefono} onChange={handleCelularChange}
+                                        />
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label htmlFor="formGroupInputCorreo" class="form-label">Correo</label>
+                                        <input type="text" className="form-control" id="nameInput"
+                                            placeholder="Ingrese su correo" value={correo} onChange={handleCorreoChange}
+                                        />
+                                    </div>
+                                    <div class="mb-3">
+                                        <label htmlFor="autoSizingSelect" class="form-label">Sede: </label>
+
+                                    </div>
+                                    <div class="mb-3">
+                                        <select id="mySelect" value={sede} onChange={handleSedeChange}>
+                                            <option value="">Seleccionar</option>
+                                            <option value="1">Cartago</option>
+                                            <option value="2">Alajuela</option>
+                                            <option value="3">Limon</option>
+                                        </select>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label htmlFor="autoSizingEstado" class="form-label">Estado: </label>
+
+                                    </div>
+                                    <div class="mb-3">
+                                        <select id="mySelect2" value={estado} onChange={handleEstadoChange}>
+                                            <option value="">Seleccionar</option>
+                                            <option value="1">Activo</option>
+                                            <option value="2">Inactivo</option>
+                                        </select>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Modificar información</button>
+                                </div>
                             </div>
-                        </div>
                         </form>
-                        <div className="mb-5">
-                            
-                        </div>
                     </div>
                 </div>
-        </div>
+            </div>
 
-    </Fragment>
-  )
+        </Fragment>
+    )
 }
