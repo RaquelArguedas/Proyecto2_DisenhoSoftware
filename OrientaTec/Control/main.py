e0b47ae2439ed044297de159655e0442b59b6521
RaquelArguedas <91703769+RaquelArguedas@users.noreply.github.com>
2023-05-10 10:21:32 -0600

    Revert "Merge branch 'Raque' of https://github.com/RaquelArguedas/Proyecto2_DisenhoSoftware into Raque"
    This reverts commit c981e0b90647fcac6d37b3aa819e39e23cd3f6e5, reversing
    changes made to d4731827f8913010afd62e9f7e479bcf645123b5.

--- OrientaTec/Control/main.py
+++ OrientaTec/Control/main.py
@@ -1,23 +1,34 @@
-# from SingletonDAO import *
-# from MainController import *
-# from datetime import time
-# from SingletonSesionActual import *
-# import json
+from SingletonDAO import *
+from MainController import *
+from datetime import time
+from SingletonSesionActual import *
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
     
-#     control = MainController()
+    control = MainController()
+    #esto solamente lo puse para que vean que el singleton funciona
+    if id(control.controlActividades.dao) == id(control.controlEquipoGuia.dao):
+        print("Funciona")
+    else:
+        print("sad")
 
 
-#     e=control.buscarEstudiante(20198)
+    print("\n")
+    dao = SingletonDAO()
 
-#     jsonStr = json.dumps(e.__dict__)
-#     print (jsonStr)
+    estudiantes = control.consultarEstudiantes(Ordenamiento.SEDE)
+
+    estudiantes = control.consultarEstudiantes(Ordenamiento.CARNET)
+    
+    estudiantes = control.consultarEstudiantes(Ordenamiento.ALFABETICAMENTE)
+
+
+    for estudiante in estudiantes:
+        print(str(estudiante))
+
     
 
-    
-
 
     
-#     #control.consultarEstudiantes(int(Ordenamiento['SEDE']))
+    #control.consultarEstudiantes(int(Ordenamiento['SEDE']))
     
