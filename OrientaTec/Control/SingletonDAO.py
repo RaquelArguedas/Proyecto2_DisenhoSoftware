a394caa77b58c68dcbc9b3523622589ebfb9d53f
Alonso Garita <45644676+alonso-gg@users.noreply.github.com>
2023-05-09 20:41:26 -0600

    Listo Calisto

--- OrientaTec/Control/SingletonDAO.py
+++ OrientaTec/Control/SingletonDAO.py
@@ -244,7 +244,7 @@ class SingletonDAO(metaclass=SingletonMeta):
                 host = 'localhost',
                 port = 3306,
                 user = 'root',
-                password = '123456',
+                password = 'abd00123',
                 db = 'orientatec'
             )
             if self.connection.is_connected():
