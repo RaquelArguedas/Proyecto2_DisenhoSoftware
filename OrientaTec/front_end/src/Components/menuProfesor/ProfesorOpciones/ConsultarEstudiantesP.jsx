4d48a7571d10faac81c227a8610a4705401dd686
Alonso Garita <45644676+alonso-gg@users.noreply.github.com>
2023-05-11 15:41:52 -0600

    stash

--- OrientaTec/front_end/src/Components/menuProfesor/ProfesorOpciones/ConsultarEstudiantesP.jsx
+++ OrientaTec/front_end/src/Components/menuProfesor/ProfesorOpciones/ConsultarEstudiantesP.jsx
@@ -1,33 +1,48 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useRef, useState } from 'react'
 import { BarraLateral } from '../../navegacion/BarraLateral'
 import { Navbar } from '../../navegacion/Navbar'
 import { FilaEstudiante } from '../../menuAsistentes/AsistenteOpciones/columnasTablas/FilaEstudiante'
 import { Icon } from '@iconify/react';
 
-const API = process.env.REACT_APP_API;
+const API = 'http://localhost:5000'; //process.env.REACT_APP_API;
 
 export function ConsultarEstudiantesP() {
+    const carnetRef = useRef();
+    const [estudiantes, setEstudiantes] = useState([]);
 
     const handleSubmit = async (event) => {
-        event.preventDefault();  
-        
-        // const res = await fetch(`${API}/getEstudiante/${20198}`, { //busca estudiante por carnet, 20198 es el carnet
-        //     method: "GET",
-        //     headers: {
-        //       "Content-Type": "application/json",
-        //     }
-        // });
+        event.preventDefault();
 
-        const res = await fetch(`${API}/consultarEstudiantes/${1}`, { //buscar por enum, 1 es el enum
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-            }
-        });
+        if (carnetRef.current.value === '') {
+            const res = await fetch(`${API}/consultarEstudiantes/${1}`, { //buscar por enum, 1 es el enum
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            });
 
-        const data = await res.json() //resultado de la consulta
-        console.log(data) // imprime en consola web
+            const data = await res.json();
+            console.log(data);
+ 
+        }/*else {
+            const res = await fetch(`${API}/getEstudiante/${carnetRef.current.value}`, { //busca estudiante por carnet, 20198 es el carnet
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            });
+
+            const data = await res.json();
+            console.log(data);
+
+            setEstudiantes((prevEstudiantes)  => {
+                return [... prevEstudiantes, datum]
+            })
+        }*/
+
+        console.log('a', estudiantes);
     }
+
     return (
         <Fragment>
             <div className="container">
@@ -43,46 +58,46 @@ export function ConsultarEstudiantesP() {
                         <form onSubmit={handleSubmit}>
                             <div class="col-12">
                                 <label for="inputCarnet" class="form-label">Carnet:</label>
-                                <input type="text" class="form-control" id="inputCarnet" placeholder="Ingrese el número de carnet a buscar"/>
+                                <input ref={carnetRef} type="text" class="form-control" id="inputCarnet" placeholder="Ingrese el número de carnet a buscar" />
                             </div>
 
                             <div className="row mt-4">
                                 <div className="col">
                                     <div className="form-check">
-                                    <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"defaultChecked />
-                                    <label className="form-check-label" htmlFor="flexRadioDefault1">
-                                        Orden alfabético
-                                    </label>
+                                        <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" defaultChecked />
+                                        <label className="form-check-label" htmlFor="flexRadioDefault1">
+                                            Orden alfabético
+                                        </label>
                                     </div>
                                 </div>
                                 <div className="col">
                                     <div className="form-check">
-                                    <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
-                                    <label className="form-check-label" htmlFor="flexRadioDefault2">
-                                        Orden por campus
-                                    </label>
+                                        <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
+                                        <label className="form-check-label" htmlFor="flexRadioDefault2">
+                                            Orden por campus
+                                        </label>
                                     </div>
                                 </div>
                                 <div className="col">
                                     <div className="form-check">
-                                    <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3" />
-                                    <label className="form-check-label" htmlFor="flexRadioDefault3">
-                                        Orden por número de carnet
-                                    </label>
+                                        <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3" />
+                                        <label className="form-check-label" htmlFor="flexRadioDefault3">
+                                            Orden por número de carnet
+                                        </label>
                                     </div>
                                 </div>
                                 <div className="col">
                                     <div className="form-check">
-                                    <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault4" />
-                                    <label className="form-check-label" htmlFor="flexRadioDefault4">
-                                        Por solo un estudiante
-                                    </label>
+                                        <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault4" />
+                                        <label className="form-check-label" htmlFor="flexRadioDefault4">
+                                            Por solo un estudiante
+                                        </label>
                                     </div>
                                 </div>
                                 <div className="mb-3">
                                     <button type="submit" class="btn btn-primary" > <Icon icon="ic:baseline-search" width="24" height="24" /> Buscar</button>
                                 </div>
-                            
+
                             </div>
                         </form>
 
@@ -99,14 +114,9 @@ export function ConsultarEstudiantesP() {
                                     </tr>
                                 </thead>
                                 <tbody id="tblEstudiantes" style={{ background: "white" }}>
-                                    <FilaEstudiante />
-                                    <FilaEstudiante />
-                                    <FilaEstudiante />
-                                    <FilaEstudiante />
-                                    <FilaEstudiante />
-                                    <FilaEstudiante />
-                                    <FilaEstudiante />
-                                    <FilaEstudiante />
+                                    {estudiantes.map((estudiante) => (
+                                        <FilaEstudiante />
+                                    ))}
                                 </tbody>
                             </table>
                         </div>
