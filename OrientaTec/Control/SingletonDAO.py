08a1fb9f223d397ab79b4001f3b23d167f5efbc0
Stacy20 <stacych30@estudiantec.cr>
2023-05-13 23:37:17 -0600

    Intento de agregar la informacion en la pantalla de plan de trabajo

--- OrientaTec/Control/SingletonDAO.py
+++ OrientaTec/Control/SingletonDAO.py
@@ -244,7 +244,7 @@ class SingletonDAO(metaclass=SingletonMeta):
                 host = 'localhost',
                 port = 3306,
                 user = 'root',
-                password = 'abd00123',
+                password = 'Michelle.18',
                 db = 'orientatec'
             )
             if self.connection.is_connected():
