3219a4c0003f1cf9afc9871cf69beaed8478a509
RaquelArguedas <91703769+RaquelArguedas@users.noreply.github.com>
2023-06-15 22:05:42 -0600

    Update SingletonDAO.py

--- OrientaTec/Control/SingletonDAO.py
+++ OrientaTec/Control/SingletonDAO.py
@@ -321,7 +321,7 @@ class SingletonDAO(metaclass=SingletonMeta):
                 host = '127.0.0.1',
                 port = 3306,
                 user = 'root',
-                password = 'abd00123',
+                password = '123456',
                 db = 'orientatec'
             )
             if self.connection.is_connected():
